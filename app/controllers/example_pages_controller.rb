c0575cf7ad827f70b193a0f350c4f38b10fecfd9
dani <dani.zaghian@gmail.com>
2022-01-10 10:48:57 -0800

    adds dynamic ruby time

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,7 +2,11 @@ class ExamplePagesController < ApplicationController
   
   def hello_method
     # the web response
-    render json: {message: "Hello World"}
+    render json: {message: "Hello World", time: Time.now.strftime("%b %e, %l:%M %p")}
+  end
+
+  def goodbye_method
+    render json: {message: "Farewell!"}
   end
 
 end
